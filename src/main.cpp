a3533738a8e438c83d1de9e5e29c3089bc71fcea
Tien Manh Nguyen <manh94@gmail.com>
2016-10-06 01:43:23 +0200

    [main] simple superpixel functions

--- src/main.cpp
+++ src/main.cpp
@@ -38,29 +38,32 @@ int main(int argc, char **argv) {
     std::cerr << "Please provide the image" << std::endl;
     return -1;
   }
-  // Input image:
+  // Initialisation:
   cv::Mat image_input = cv::imread(argv[1], cv::IMREAD_COLOR);
   cv::Mat image_result;
-  
+  cv::Mat mask;
   cv::Ptr<cv::ximgproc::SuperpixelSLIC> slic_image;
-  //slic_image->iterate(10);
+  //
 
   // Apply the superpixel to the image:
-  slic_image = cv::ximgproc::createSuperpixelSLIC(image_input, 100, 10, 10.0);
-  //cv::ximgproc::SuperpixelSLICImpl(image_input,100,10,10.0);
+  slic_image = cv::ximgproc::createSuperpixelSLIC(image_input, 101, 10, 10.0);
+  slic_image->iterate(10);
   
   // Number of superpixel:
   int nb_superpixel = slic_image->getNumberOfSuperpixels();
 
   std::cout << " Number of superpixels is : " << nb_superpixel << std::endl;
   
-  // get the pixel:
-  //slic_image->getLabels(image_result);
-  
-  //cv::cvtColor(image_input, image_result, CV_RGB2GRAY);
-  // Show the result:
-  image_result = image_input;
-  const cv::Size display_size = cv::Size(600,400);
+  image_input.copyTo(image_result);
+
+  // get the contours for displaying:
+  slic_image->getLabelContourMask(mask, true);
+
+  // Set mask to the result image:
+  image_result.setTo(cv::Scalar(0, 0, 255), mask);
+
+    // Show the result:
+  const cv::Size display_size = cv::Size(800,600);
   display_matrix("Original", image_input,
 		 display_size, true);
   display_matrix("Result", image_result,
